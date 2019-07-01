1117d0487fdab6b1148f155236d5fa40e5446d3a
Cameron Welter <camjwelt@gmail.com>
2019-07-01 05:18:02 +0000

    add customized theme

---
+++ src/clapton/build.js
@@ -0,0 +1,15 @@
+/* eslint-disable global-require, import/no-dynamic-require, import/no-extraneous-dependencies */
+const fs = require('fs')
+const mustache = require('mustache')
+
+const globals = require('../dark/globals.json')
+
+const template = JSON.stringify(require(`${__dirname}/template.json`))
+
+const themeNames = ['horizon-clapton']
+
+themeNames.forEach((themeName) => {
+  const content = require(`${__dirname}/${themeName}.json`)
+  const theme = mustache.render(template, { ...content, ...globals })
+  fs.writeFileSync(`${__dirname}/../../themes/${themeName}.json`, theme)
+})
